f0c4cc2294c24ace95cbfb2f7f43933532a40cb7
WhoDis2372 <uneasy-glade-flip@duck.com>
2024-12-09 10:35:53 +0200

    Add public key for agenix

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,8 @@
 let
   a1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpmdkxAZH3See4FaPgiZoTxpcEC1orOGB9cXoQtYUYI";
+  a2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiiNmXma1IijIE6U6CKsGcmfGf0gqSZ5S0fvZABv+tA";
 
-  admins = [ a1 ];
+  admins = [ a1 a2 ];
 
   vm-admin = (builtins.readFile ./hosts/vm-admin/ssh_host_ed25519_key.pub);
   vm-minimal = (builtins.readFile ./hosts/vm-minimal/ssh_host_ed25519_key.pub);
