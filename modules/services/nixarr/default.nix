e8e29cb948700fbcdff14c6eaa4b02d74e4ed6d7
WhoDis2372 <uneasy-glade-flip@duck.com>
2024-12-10 16:09:32 +0200

    Init simple nixarr config

--- modules/services/nixarr/default.nix
+++ modules/services/nixarr/default.nix
@@ -3,4 +3,32 @@
   imports = [
     inputs.nixarr.nixosModules.default
   ];
+
+  nixarr = {
+    enable = true;
+    mediaDir = "/data/media";
+    stateDir = "/data/media/.state/nixarr";
+
+    jellyfin.enable = true;
+
+    transmission = {
+      enable = true;
+      peerPort = 34789;
+    };
+
+    bazarr.enable = true;
+    lidarr.enable = true;
+    prowlarr.enable = true;
+    radarr.enable = true;
+    readarr.enable = true;
+    sonarr.enable = true;
+  };
+
+  # Sonarr runs dotnet 6...
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-6.0.428"
+    "dotnet-sdk-wrapped-6.0.428"
+  ];
 }
