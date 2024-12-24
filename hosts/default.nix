13cb1a6449cab10a762664a7dc9b81089de94584
WhoDis2372 <uneasy-glade-flip@duck.com>
2024-12-24 10:32:17 +0200

    Add cachix binary cache to hosts

--- hosts/default.nix
+++ hosts/default.nix
@@ -67,6 +67,14 @@
           "root"
           "${username}"
         ];
+        # Cachix
+        # TODO: DRY (this repeats in flake.nix)
+        substituters = [
+          "https://arc2-group.cachix.org"
+        ];
+        trusted-public-keys = [
+          "arc2-group.cachix.org-1:SfZ4Amg/VroYhmCRNX0mQcFEWGCFWvn31s3gwEaU/2U="
+        ];
       };
       # Disable channels
       channel.enable = false;
