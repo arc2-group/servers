20b69e2dd75f4e9d140a5a5034a71383dc12f86b
Luna <techyluna@proton.me>
2025-01-15 23:04:10 +0200

    main setup for nextcloud

---
+++ modules/services/nextcloud/default.nix
@@ -0,0 +1,38 @@
+{ pkgs, config, ... }:
+{
+  services.nextcloud = {
+    enable = true;
+    hostName = "nc.example.com"; # Enter your domain here
+    package = pkgs.nextcloud30; # Need to manually increment with every major upgrade.
+    home = /data;
+    datadir = /var/lib/nexctcloud;
+    database.createLocally = true; # Let NixOS install and configure the database automatically.
+    configureRedis = true; # Let NixOS install and configure Redis caching automatically.
+    maxUploadSize = "16G";
+    autoUpdateApps.enable = false;
+    extraAppsEnable = true;
+    extraApps = with config.services.nextcloud.package.packages.apps; {
+      # List of already packaged apps:
+      # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
+      inherit calendar forms polls richdocuments files_markdown files_texteditor mail;
+      ## Custom app example
+      #socialsharing_telegram = pkgs.fetchNextcloudApp rec {
+      #  url =
+      #    "https://github.com/nextcloud-releases/socialsharing/releases/download/v3.0.1/socialsharing_telegram-v3.0.1.tar.gz";
+      #  license = "agpl3";
+      #  sha256 = "sha256-8XyOslMmzxmX2QsVzYzIJKNw6rVWJ7uDhU1jaKJ0Q8k=";
+      #};
+    };
+    settings = {
+      overwriteprotocol = "https";
+      log_type = "syslog";
+      loglevel = 1;
+    };
+    config = {
+      dbtype = "pgsql";
+      # adminuser = "admin";
+      # adminpassFile = "/var/nc_admin_pass";
+    };
+    phpOptions."opcache.interned_strings_buffer" = "16";
+  };
+}
