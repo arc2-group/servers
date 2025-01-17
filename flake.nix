60af47341fcf302054c9fbdf95adb9097b720f9b
Luna <techyluna@proton.me>
2025-01-17 02:37:40 +0200

    removed override

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     conduwuit.url = "github:NixOS/nixpkgs/nixos-unstable";
-    conduwuit.inputs.nixpkgs.follows = "nixpkgs";
 
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
