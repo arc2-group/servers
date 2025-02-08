f149ad9ca0f7e941b3cde6b00bee18825adf6b76
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-02-08 15:31:10 +0200

    media: forward IPv6 to IPv4

--- modules/services/nixarr/default.nix
+++ modules/services/nixarr/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
     inputs.nixarr.nixosModules.default
@@ -33,4 +33,22 @@
     "dotnet-sdk-6.0.428"
     "dotnet-sdk-wrapped-6.0.428"
   ];
+
+  # Jellyfin and Transmission don't listen on IPv6 (WARN: bodge)
+  systemd.services.socat-bridge-jellyfin = {
+    description = "Socat bridge for IPv4 to IPv6";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat TCP6-LISTEN:18096,fork TCP4:127.0.0.1:8096";
+      Restart = "always";
+    };
+  };
+  systemd.services.socat-bridge-transmission = {
+    description = "Socat bridge for IPv4 to IPv6";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat TCP6-LISTEN:19091,fork TCP4:127.0.0.1:9091";
+      Restart = "always";
+    };
+  };
 }
