1a7ae12423854f2082fbd7814c79d0602b785bc5
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-02-13 15:16:03 +0200

    nginx: +grafana vhost

---
+++ modules/services/nginx/vhosts/grafana.nix
@@ -0,0 +1,23 @@
+{ outputs, ... }:
+let
+  grafanaPort =
+    outputs.nixosConfigurations.vm-monitoring.config.services.grafana.settings.server.http_port;
+in
+{
+  services.nginx.virtualHosts = {
+    "grafana.blazma.st" = {
+      forceSSL = true;
+      enableACME = true;
+
+      locations."/" = {
+        proxyPass = "http://vm-monitoring:${toString grafanaPort}";
+      };
+
+      extraConfig = ''
+        if ($ssl_client_verify != SUCCESS) {
+          return 403;
+        }
+      '';
+    };
+  };
+}
