1a59e68436607281a7a0a80a89652a912174ebc3
Warden2915 <cash-dilute-jurist@duck.com>
2025-02-14 19:56:04 +0200

    navidrome: init with lastfm working

--- modules/services/navidrome/default.nix
+++ modules/services/navidrome/default.nix
@@ -1,15 +1,22 @@
+{ hostname, config, ... }:
+let
+  secrets = "${hostname}-navidrome-secrets";
+in
 {
   services.navidrome = {
     enable = true;
+    openFirewall = true;
+    group = "media";
     settings = {
       MusicFolder = "/data/library/music";
       DataFolder = "/navidrome";
+      Address = "[::]";
 
       # someone is against setting this to false
       # EnableStarRating = false;
       # TODO update this someone
-      #LastFM.ApiKey = "";
-      #LastFM.Secret = "";
+      # navidrome devs did not cook with these things
+      # https://github.com/navidrome/navidrome/discussions/3463
       #PasswordEncryptionKey = "";
       #Prometheus.Enabled = "";
       #Prometheus.MetricsPath = "";
@@ -25,5 +32,15 @@
 
   networking.firewall = {
     allowedTCPPorts = [ 4533 ];
+  };
+
+  age.secrets.${secrets} = {
+    file = ./secrets.env.age;
+  };
+
+  # continuation of the lastfm secrets mess
+  systemd.services.navidrome.serviceConfig = {
+    EnvironmentFile = config.age.secrets.${secrets}.path;
+    BindReadOnlyPaths = "/etc/resolv.conf";
   };
 }
