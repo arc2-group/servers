e279816574853e2c29e492ed5c5102939ef16d9b
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-02-21 17:11:16 +0200

    vm-minimal: remove

--- secrets.nix
+++ secrets.nix
@@ -10,7 +10,6 @@ let
   ];
 
   vm-admin = builtins.readFile ./hosts/vm-admin/ssh_host_ed25519_key.pub;
-  vm-minimal = builtins.readFile ./hosts/vm-minimal/ssh_host_ed25519_key.pub;
   vm-public-ingress = builtins.readFile ./hosts/vm-public-ingress/ssh_host_ed25519_key.pub;
   vm-public-media = builtins.readFile ./hosts/vm-public-media/ssh_host_ed25519_key.pub;
   vm-public-matrix = builtins.readFile ./hosts/vm-public-matrix/ssh_host_ed25519_key.pub;
@@ -18,15 +17,12 @@ let
 
   systems = [
     vm-admin
-    vm-minimal
   ];
   everyone = systems ++ admins;
 in
 {
   "hosts/vm-admin/id_ed25519.age".publicKeys = everyone;
   "hosts/vm-admin/ssh_host_ed25519_key.age".publicKeys = everyone;
-
-  "hosts/vm-minimal/ssh_host_ed25519_key.age".publicKeys = everyone;
 
   "hosts/vm-public-ingress/ssh_host_ed25519_key.age".publicKeys = [ vm-public-ingress ] ++ everyone;
 
