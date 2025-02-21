e4e05d59548d1e689f21802f10bfef7cbac65c83
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-02-21 17:41:35 +0200

    secrets.nix: reorganize & DRY

--- secrets.nix
+++ secrets.nix
@@ -1,37 +1,47 @@
 let
-  a1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpmdkxAZH3See4FaPgiZoTxpcEC1orOGB9cXoQtYUYI";
-  a2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiiNmXma1IijIE6U6CKsGcmfGf0gqSZ5S0fvZABv+tA";
-  a3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDGk2s3Q7/djy4ytAQUBpVuo+97yQz9p4tzREX+vujoy";
-
   admins = [
-    a1
-    a2
-    a3
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDGk2s3Q7/djy4ytAQUBpVuo+97yQz9p4tzREX+vujoy"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiiNmXma1IijIE6U6CKsGcmfGf0gqSZ5S0fvZABv+tA"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpmdkxAZH3See4FaPgiZoTxpcEC1orOGB9cXoQtYUYI"
+    s.vm-admin
   ];
 
-  vm-admin = builtins.readFile ./hosts/vm-admin/ssh_host_ed25519_key.pub;
-  vm-public-ingress = builtins.readFile ./hosts/vm-public-ingress/ssh_host_ed25519_key.pub;
-  vm-public-media = builtins.readFile ./hosts/vm-public-media/ssh_host_ed25519_key.pub;
-  vm-public-matrix = builtins.readFile ./hosts/vm-public-matrix/ssh_host_ed25519_key.pub;
-  vm-monitoring = builtins.readFile ./hosts/vm-monitoring/ssh_host_ed25519_key.pub;
+  # Read ./hosts directory to get a list of systems
+  entries = builtins.readDir ./hosts;
+  configurations = builtins.filter (
+    name: entries.${name} == "directory" && builtins.pathExists ./hosts/${name}/ssh_host_ed25519_key.pub
+  ) (builtins.attrNames entries);
 
-  systems = [
-    vm-admin
-  ];
-  everyone = systems ++ admins;
+  # Build a set of attributes mapping system names to its public key (read from each system’s file)
+  s = builtins.listToAttrs (
+    map (name: {
+      inherit name;
+      value = builtins.readFile ./hosts/${name}/ssh_host_ed25519_key.pub;
+    }) configurations
+  );
+
+  # Generate an attribute set with keys like "hosts/<vm>/ssh_host_ed25519_key.age"
+  secrets = builtins.listToAttrs (
+    map (vm: {
+      # Here vm is just a string, the host name
+      name = "hosts/" + vm + "/ssh_host_ed25519_key.age";
+      value = {
+        publicKeys = admins ++ [ s.${vm} ];
+      };
+    }) configurations
+  );
+
 in
 {
-  "hosts/vm-admin/id_ed25519.age".publicKeys = everyone;
-  "hosts/vm-admin/ssh_host_ed25519_key.age".publicKeys = everyone;
+  "hosts/vm-admin/id_ed25519.age".publicKeys = admins;
+  "hosts/vm-admin/ssh_host_ed25519_key.age".publicKeys = admins;
 
-  "hosts/vm-public-ingress/ssh_host_ed25519_key.age".publicKeys = [ vm-public-ingress ] ++ everyone;
-
-  "hosts/vm-public-media/ssh_host_ed25519_key.age".publicKeys = [ vm-public-media ] ++ everyone;
-  "modules/services/navidrome/secrets.env.age".publicKeys = [ vm-public-media ] ++ everyone;
-
-  "hosts/vm-public-matrix/ssh_host_ed25519_key.age".publicKeys = [ vm-public-matrix ] ++ everyone;
-  "modules/services/conduwuit/registration-token.age".publicKeys = [ vm-public-matrix ] ++ everyone;
-
-  "hosts/vm-monitoring/ssh_host_ed25519_key.age".publicKeys = [ vm-monitoring ] ++ everyone;
-  "modules/services/monitoring/grafana/admin-password.age".publicKeys = [ vm-monitoring ] ++ everyone;
+  "modules/services/navidrome/secrets.env.age".publicKeys = admins ++ [ s.vm-public-media ];
+  "hosts/vm-public-matrix/ssh_host_ed25519_key.age".publicKeys = admins ++ [ s.vm-public-matrix ];
+  "modules/services/conduwuit/registration-token.age".publicKeys = admins ++ [ s.vm-public-matrix ];
+  "hosts/vm-monitoring/ssh_host_ed25519_key.age".publicKeys = admins ++ [ s.vm-monitoring ];
+  "modules/services/monitoring/grafana/admin-password.age".publicKeys = admins ++ [
+    s.vm-monitoring
+  ];
 }
+// secrets
