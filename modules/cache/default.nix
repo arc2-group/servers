a42a9e3624a76453133087d385ecd2227c2c4ae4
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-02-27 11:11:22 +0200

    remove duplicate binary caches

--- modules/cache/default.nix
+++ modules/cache/default.nix
@@ -3,14 +3,10 @@ _: {
     substituters = [
       "https://arc2-group.cachix.org"
       "https://attic.kennel.juneis.dog/conduwuit"
-      "https://attic.kennel.juneis.dog/conduit"
-      "https://conduwuit.cachix.org"
     ];
     trusted-public-keys = [
       "arc2-group.cachix.org-1:SfZ4Amg/VroYhmCRNX0mQcFEWGCFWvn31s3gwEaU/2U="
       "conduwuit:BbycGUgTISsltcmH0qNjFR9dbrQNYgdIAcmViSGoVTE="
-      "conduit:eEKoUwlQGDdYmAI/Q/0slVlegqh/QmAvQd7HBSm21Wk="
-      "conduwuit.cachix.org-1:MFRm6jcnfTf0jSAbmvLfhO3KBMt4px+1xaereWXp8Xg="
     ];
   };
 }
