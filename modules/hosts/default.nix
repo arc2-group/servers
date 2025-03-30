d9abc7cbf06bedfa9a7016f7ce458d5e99f70fa4
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-03-30 11:59:34 -0300

    nginx/ltvg: add stream proxy

--- modules/hosts/default.nix
+++ modules/hosts/default.nix
@@ -5,5 +5,6 @@ _: {
     "fd5e:934f:acab:1::6002" = [ "vm-public-media" ];
     "fd5e:934f:acab:1::6005" = [ "vm-public-matrix" ];
     "fd5e:934f:acab:1::6006" = [ "vm-monitoring" ];
+    "fd5e:934f:acab:2::8001" = [ "ext-ltvg" ];
   };
 }
--- modules/services/nginx/vhosts/ltvg.nix
+++ modules/services/nginx/vhosts/ltvg.nix
@@ -5,9 +5,29 @@ _: {
       enableACME = true;
 
       locations."/" = {
-        proxyPass = "https://[fd5e:934f:acab:2::8001]";
+        proxyPass = "https://ext-ltvg";
         proxyWebsockets = true;
       };
     };
+  };
+
+  services.nginx.streamConfig = ''
+    server {
+      listen 0.0.0.0:8005;
+      proxy_timeout 20s;
+      proxy_pass ext-ltvg:8005;
+    }
+    server {
+      listen 0.0.0.0:8006;
+      proxy_timeout 20s;
+      proxy_pass ext-ltvg:8006;
+    }
+  '';
+
+  networking.firewall = {
+    allowedTCPPorts = [
+      8005
+      8006
+    ];
   };
 }
