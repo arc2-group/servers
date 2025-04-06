2e2420ef70045d7ac469ac4cd46a47e6771fab6d
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-04-07 00:34:02 +0300

    nginx/matrix: increase timeouts & max body size

--- modules/services/nginx/vhosts/matrix.nix
+++ modules/services/nginx/vhosts/matrix.nix
@@ -6,6 +6,17 @@ _: {
 
       locations."/" = {
         proxyPass = "http://vm-public-matrix:6167";
+
+        extraConfig = ''
+          # increase buffer size and timeouts to allow for longer & larger syncs
+          proxy_buffers 128 64k; # 8M
+          proxy_read_timeout 15m;
+          proxy_send_timeout 15m;
+          keepalive_timeout 15m;
+
+          # match max upload size with conduwuit settings
+          client_max_body_size 100M;
+        '';
       };
     };
   };
