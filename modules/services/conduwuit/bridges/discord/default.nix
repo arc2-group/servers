8d68dccc7238d8aa9134f2915eb688bb48faa1af
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-04-09 08:53:14 +0300

    mautrix-discord: backfill

--- modules/services/conduwuit/bridges/discord/default.nix
+++ modules/services/conduwuit/bridges/discord/default.nix
@@ -22,6 +22,21 @@ _: {
           "@naides3:blazma.st" = "admin";
           "@spur:blazma.st" = "admin";
         };
+        backfill = {
+          forward_limits = {
+            initial = {
+              dm = 100;
+              channel = 200;
+              thread = 100;
+            };
+            missed = {
+              dm = -1;
+              channel = -1;
+              thread = -1;
+            };
+            max_guild_members = -1;
+          };
+        };
       };
     };
 
