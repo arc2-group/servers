22ae9282098ddb55912ac5ea8116d5d3be056c68
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-04-21 07:44:47 +0300

    vm-public-media: separate state disk

--- hosts/vm-public-media/default.nix
+++ hosts/vm-public-media/default.nix
@@ -3,6 +3,21 @@ args: {
     ../../modules/services/navidrome
     ../../modules/services/nixarr
     ../../modules/services/slskd
-    (import ../../modules/disks/data.nix (args // { mountpoint = "/data"; }))
+
+    (import ../../modules/disks/data.nix (
+      args
+      // {
+        mountpoint = "/media";
+        device = "/dev/disk/by-path/pci-0000:01:02.0-scsi-0:0:0:1";
+      }
+    )) # media
+
+    (import ../../modules/disks/data.nix (
+      args
+      // {
+        mountpoint = "/var";
+        device = "/dev/disk/by-path/pci-0000:01:03.0-scsi-0:0:0:2";
+      }
+    )) # state + program data
   ];
 }
