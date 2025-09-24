2803841237160c6dbb4d7c8d3e0261a810ebb7f7
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-09-24 15:33:25 +0300

    mautrix-whatsapp: update config layout

--- modules/services/continuwuity/bridges/whatsapp/default.nix
+++ modules/services/continuwuity/bridges/whatsapp/default.nix
@@ -1,9 +1,17 @@
-_: {
+{ inputs, ... }:
+{
+  # https://github.com/NixOS/nixpkgs/pull/420722
+  disabledModules = [
+    "services/matrix/mautrix-whatsapp.nix"
+  ];
+  imports = [
+    "${inputs.nixpkgs_unstable}/nixos/modules/services/matrix/mautrix-whatsapp.nix"
+  ];
+
   services.mautrix-whatsapp = {
     enable = true;
     registerToSynapse = false;
     settings = {
-      apply = true;
       homeserver = {
         domain = "blazma.st";
         address = "http://[::1]:6167";
@@ -11,38 +19,40 @@ _: {
       appservice = {
         id = "whatsapp";
       };
+      matrix = {
+        delivery_receipts = true;
+      };
+      backfill = {
+        enable = true;
+      };
+      encryption = {
+        allow = true;
+        default = true;
+        delete_keys = {
+          dont_store_outbound = true;
+          ratchet_on_decrypt = true;
+          delete_fully_used_on_decrypt = true;
+          delete_prev_on_new_session = true;
+          delete_on_device_delete = true;
+          periodically_delete_expired = true;
+        };
+        verification_levels = {
+          receive = "cross-signed-tofu";
+          send = "cross-signed-tofu";
+          share = "cross-signed-tofu";
+        };
+      };
+      network = {
+        history_sync = {
+          max_initial_conversations = 10;
+        };
+      };
       bridge = {
         permissions = {
           "*" = "relay";
           "blazma.st" = "user";
           "@naides3:blazma.st" = "admin";
           "@spur:blazma.st" = "admin";
-        };
-        history_sync = {
-          max_initial_conversations = 10;
-        };
-        matrix = {
-          delivery_receipts = true;
-        };
-        backfill = {
-          enable = true;
-        };
-        encryption = {
-          allow = true;
-          default = true;
-          delete_keys = {
-            dont_store_outbound = true;
-            ratchet_on_decrypt = true;
-            delete_fully_used_on_decrypt = true;
-            delete_prev_on_new_session = true;
-            delete_on_device_delete = true;
-            periodically_delete_expired = true;
-          };
-          verification_levels = {
-            receive = "cross-signed-tofu";
-            send = "cross-signed-tofu";
-            share = "cross-signed-tofu";
-          };
         };
       };
     };
