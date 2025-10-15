aa8290885afa712c5ef0f77b5eb4642334544878
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-10-15 12:35:39 +0300

    nginx/qrp: add server aliases

--- modules/services/nginx/vhosts/qrp.nix
+++ modules/services/nginx/vhosts/qrp.nix
@@ -3,6 +3,10 @@ _: {
     "pasakumi.kviriem.lv" = {
       forceSSL = true;
       enableACME = true;
+      serverAliases = [
+        "kviriem.lv"
+        "www.kviriem.lv"
+      ];
 
       locations."/" = {
         proxyPass = "http://vm-public-qrp";
