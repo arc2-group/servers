ef9908d30f252a19ef748f0c499f7cf1fb5669e3
WhoDis2372 <uneasy-glade-flip@duck.com>
2025-10-24 10:02:46 +0300

    vm-public-cloud: init

---
+++ hosts/vm-public-cloud/default.nix
@@ -0,0 +1,11 @@
+args: {
+  imports = [
+    (import ../../modules/disks/data.nix (
+      args
+      // {
+        mountpoint = "/var";
+        device = "/dev/disk/by-path/pci-0000:01:03.0-scsi-0:0:0:2";
+      }
+    )) # state + program data
+  ];
+}
