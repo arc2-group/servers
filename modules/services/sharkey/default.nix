aec3c16e2ceb9c5782c2f34a675ef69034e61df2
WhoDis2372 <uneasy-glade-flip@duck.com>
2026-03-09 07:05:23 +0200

    sharkey: update package

--- modules/services/sharkey/default.nix
+++ modules/services/sharkey/default.nix
@@ -1,6 +1,18 @@
-_: {
+{ pkgs, ... }:
+{
   services.sharkey = {
     enable = true;
+    package = pkgs.sharkey.overrideAttrs {
+      version = "2025.4.6";
+      src = pkgs.fetchFromGitLab {
+        domain = "activitypub.software";
+        owner = "TransFem-org";
+        repo = "Sharkey";
+        tag = "2025.4.6";
+        hash = "sha256-TtwlveTIjzDYpFR+F5c0If6E1D2E5MI9I2IoDIV0u7E=";
+        fetchSubmodules = true;
+      };
+    };
     openFirewall = true;
     setupRedis = true;
     setupPostgresql = true;
